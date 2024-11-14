3ae5a2e5e00267628f0b738b02516d27d9c23297
Ramsha Trannum <ramshatarannum07@gmail.com>
2024-11-14 16:04:52 +0530

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,4 +95,6 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  # config/environments/production.rb
+  config.secret_key_base = ENV['SECRET_KEY_BASE']
 end
