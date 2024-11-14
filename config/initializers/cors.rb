fbd7640e600450539ee56a6ae0882cc8d5b80bad
ramsha <amshatarannum07@gmail.com>
2024-11-14 19:27:44 +0530

    fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://frabjous-praline-cf7fda.netlify.app/'
+    origins 'http://frabjous-praline-cf7fda.netlify.app/'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
